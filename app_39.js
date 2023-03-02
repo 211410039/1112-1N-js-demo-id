85f2a6274d1af1a89b743d3ed3c59ad420115c93
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-02 21:29:36 +0800

    W03-P6: add your info as reviews[0] and show it

--- app_39.js
+++ app_39.js
@@ -2,27 +2,34 @@
 const reviews = [
   {
     id: 1,
+    name: 'wang lin ',
+    job: 'student',
+    img: 'https://qmfqlvkbasosvmqhicrw.supabase.co/storage/v1/object/public/demo-39/md_img/my.jpg?t=2023-03-02T13%3A24%3A54.857Z',
+    text: "I'm a nice person.",
+  },
+  {
+    id: 2,
     name: 'susan smith',
     job: 'web developer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
-    id: 2,
+    id: 3,
     name: 'anna johnson',
     job: 'web designer',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
   },
   {
-    id: 3,
+    id: 4,
     name: 'peter jones',
     job: 'intern',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
     text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
   },
   {
-    id: 4,
+    id: 5,
     name: 'bill anderson',
     job: 'the boss',
     img: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
