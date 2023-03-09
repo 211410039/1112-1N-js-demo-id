30e66fc9e21f010dc07456dc77bedd5c214cef68
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-09 20:10:44 +0800

    W04-P2: show all data in array menu into html (section-center)

--- app_39.js
+++ app_39.js
@@ -4,34 +4,34 @@ const menu = [
         title: 'french toast',
         category: 'breakfast',
         price: 10.52,
-        image: './images/item-1.jpeg',
+        img: './images/item-1.jpeg',
         remote_img: '',
-        desc: 'strawberrys,oranges and french toast on plates'
+        desc: 'strawberrys,oranges and french toast on plates smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
     },
     {
         id: 2,
         title: 'salad',
         category: 'lunch',
         price: 13.25,
-        image: './images/item-2.jpeg',
+        img: './images/item-2.jpeg',
         remote_img: '',
-        desc: 'vegetable salad'
+        desc: 'vegetable salad smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
     },
     {
         id: 3,
         title: 'steak',
         category: 'dinner',
         price: 16.38,
-        image: './images/item-3.jpeg',
+        img: './images/item-3.jpeg',
         remote_img: '',
-        desc: 'fried food and steak on black pan'
+        desc: 'fried food and steak on black pan smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
     },
     {
         id: 4,
         title: 'buttermilk pancakes',
         category: 'breakfast',
         price: 15.59,
-        image: './images/item-4.jpeg',
+        img: './images/item-4.jpeg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
     },
@@ -40,7 +40,7 @@ const menu = [
         title: 'dinner double',
         category: 'dinner',
         price: 13.95,
-        image: './images/item-5.jpeg',
+        img: './images/item-5.jpeg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
     },
@@ -49,7 +49,7 @@ const menu = [
         title: 'godzilla milkshake',
         category: 'shakes',
         price: 13.99,
-        image: './images/item-6.jpeg',
+        img: './images/item-6.jpeg',
         remote_img: '',
         desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
     },
@@ -57,3 +57,29 @@ const menu = [
 
 const sectionCenter = document.querySelector('.section-center');
 const btnContainer = document.querySelector('.btn-container');
+
+const displayMenuItems = (menu) => {
+    let displayMenu = menu.map( (item) =>{
+        return `
+        <article class="menu-item">
+          <img src=${item.img} alt=${item.title} class="photo" />
+          <div class="item-info">
+            <header>
+              <h4>${item.title}</h4>
+              <h4 class="price">${item.price}</h4>
+            </header>
+            <p class="item-text">
+                ${item.desc}
+            </p>
+          </div>
+        </article>`
+    });
+    console.log('displayMenu before join', displayMenu);
+    displayMenu = displayMenu.join('');
+    console.log('displayMenu after join', displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+}
+
+window.addEventListener('DOMContentLoaded' , () => {
+    displayMenuItems(menu);
+});
