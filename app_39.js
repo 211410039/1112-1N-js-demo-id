d2161ff7b1ae699f9b165c5622ea4dc98c43e2b1
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-09 19:18:45 +0800

    W04-P1: add 3 images of my meals to menu array

---
+++ app_39.js
@@ -0,0 +1,59 @@
+const menu = [
+    {
+        id: 1,
+        title: 'french toast',
+        category: 'breakfast',
+        price: 10.52,
+        image: './images/item-1.jpeg',
+        remote_img: '',
+        desc: 'strawberrys,oranges and french toast on plates'
+    },
+    {
+        id: 2,
+        title: 'salad',
+        category: 'lunch',
+        price: 13.25,
+        image: './images/item-2.jpeg',
+        remote_img: '',
+        desc: 'vegetable salad'
+    },
+    {
+        id: 3,
+        title: 'steak',
+        category: 'dinner',
+        price: 16.38,
+        image: './images/item-3.jpeg',
+        remote_img: '',
+        desc: 'fried food and steak on black pan'
+    },
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        image: './images/item-4.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+    {
+        id: 5,
+        title: 'dinner double',
+        category: 'dinner',
+        price: 13.95,
+        image: './images/item-5.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+    {
+        id: 6,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 13.99,
+        image: './images/item-6.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+];
+
+const sectionCenter = document.querySelector('.section-center');
+const btnContainer = document.querySelector('.btn-container');
