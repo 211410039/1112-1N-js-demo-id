999e84f90f39c3b777cd9188c4ec10074603ba9d
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-16 21:54:17 +0800

    W05-P4: call closeModal to close modal, overlay; press ESC key will call closeModal as well

--- demo/w05_39-modal/script_39.js
+++ demo/w05_39-modal/script_39.js
@@ -1,26 +1,35 @@
 'use strict';
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelectorAll('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal = document.querySelectorAll('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log('modal', modal);
 console.log('show-modal', btnsOpenModal);
 
 
 const openModal = (index) => {
-    modal.classList.remove('hidden');
+    modal[index].classList.remove('hidden');
     overlay.classList.remove('hidden');
+}
+
+const closeModal = (index) => {
+    modal[index].classList.add('hidden');
+    overlay.classList.add('hidden');
 }
 
 
 for(let i=0; i<btnsOpenModal.length; i++ ){
-    btnsOpenModal[i].addEventListener('click', openModal(i));
-}
+    btnsOpenModal[i].addEventListener('click', (e) => openModal(i))
+    btnCloseModal[i].addEventListener('click', (e) => closeModal(i))
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' &&  !modal[i].classList.contains('hidden')) {
+            closeModal(i);
+        }
+    });
+    
+};
 
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'ESCAPE' &&  !modal.classList.contains('hidden')) {
-        closeModal();
-    }
-});
+
+
