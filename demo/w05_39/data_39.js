5c95748fca21b76a4e562ac247b129b7cd6787df
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-16 18:46:30 +0800

    W05-P1: use JavaScript module to import and export

---
+++ demo/w05_39/data_39.js
@@ -0,0 +1,65 @@
+export const menu = [
+    {
+        id: 1,
+        title: 'french toast',
+        category: 'breakfast',
+        price: 10.52,
+        img: './images/item-1.jpeg',
+        remote_img: '',
+        desc: 'strawberrys,oranges and french toast on plates smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
+    },
+    {
+        id: 2,
+        title: 'salad',
+        category: 'lunch',
+        price: 13.25,
+        img: './images/item-2.jpeg',
+        remote_img: '',
+        desc: 'vegetable salad smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
+    },
+    {
+        id: 3,
+        title: 'steak',
+        category: 'dinner',
+        price: 16.38,
+        img: './images/item-3.jpeg',
+        remote_img: '',
+        desc: 'fried food and steak on black pan smells good and tastes good and vary favor can choose just try it!if you are hungry, let it go in to your body! make a better day and better emotion to every continue day ! '
+    },
+    {
+        id: 4,
+        title: 'buttermilk pancakes',
+        category: 'breakfast',
+        price: 15.59,
+        img: './images/item-4.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+    {
+        id: 5,
+        title: 'dinner double',
+        category: 'dinner',
+        price: 13.95,
+        img: './images/item-5.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+    {
+        id: 6,
+        title: 'godzilla milkshake',
+        category: 'shakes',
+        price: 13.99,
+        img: './images/item-6.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+    {
+        id: 6,
+        title: 'godzilla milkshake',
+        category: 'desserts',
+        price: 13.99,
+        img: './images/item-6.jpeg',
+        remote_img: '',
+        desc: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto modi debitis numquam harum, impedit soluta tempora aliquid commodi, earum odit expedita minima id eveniet mollitia quasi libero ducimus dignissimos iste!'
+    },
+];
