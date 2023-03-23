94161ba961f4ecabc4707707a6f692d557f01cca
21141003901~ <211410039@o365.tku.edu.tw>
2023-03-23 22:06:04 +0800

    W06-P5: Temperature convert C2F(), F2C() using Web interface

---
+++ demo/w06_39-array/p3_39/p3_39.js
@@ -0,0 +1,26 @@
+const userInput = document.querySelector('#input-number');
+const cBtn = document.querySelector('#btn-c');
+const fBtn = document.querySelector('#btn-f');
+
+const currentcalculationOutput= document.querySelector('#current-calculation');
+
+function getUserInput(){
+    return parseFloat(userInput.value);//parse輸入的東西轉變型態
+}
+
+function outputResult(text){
+    currentcalculationOutput.textContent = text;
+}
+
+function f(){
+    const operand1 = getUserInput();
+    const calcText = `${operand1} `*9/5+32;
+    const outputText = `${operand1} F = ${calcText} C`;
+    outputResult(outputText);
+}
+
+fBtn.addEventListener('click',f);
+
+// let c, f;
+// f =( c * 9/5 +32 ).toFixed(2);//toFixed取小數點
+// console.log(`${c} C = ${f} F`);//輸出
