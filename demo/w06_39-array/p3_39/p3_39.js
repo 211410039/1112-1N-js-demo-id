8bc9a0134fb0dcf2eb14ff0356822f177d082a5e
211410039 <211410039@o365.tku.edu.tw>
2023-03-23 23:36:21 +0800

    w6-logs

--- demo/w06_39-array/p3_39/p3_39.js
+++ demo/w06_39-array/p3_39/p3_39.js
@@ -12,14 +12,22 @@ function outputResult(text){
     currentcalculationOutput.textContent = text;
 }
 
-function f(){
+function c(){
     const operand1 = getUserInput();
     const calcText = `${operand1} `*9/5+32;
+    const outputText = `${operand1} C = ${calcText} F`;
+    outputResult(outputText);
+}
+
+function f(){
+    const operand1 = getUserInput();
+    const calcText = ((`${operand1} `-32)*5/9).toFixed(2);
     const outputText = `${operand1} F = ${calcText} C`;
     outputResult(outputText);
 }
 
-fBtn.addEventListener('click',f);
+fBtn.addEventListener('click',c);
+cBtn.addEventListener('click',f);
 
 // let c, f;
 // f =( c * 9/5 +32 ).toFixed(2);//toFixed取小數點
