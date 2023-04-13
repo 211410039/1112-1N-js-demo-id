c67d6dfd01b213189c5559ff2b1505c0652b808f
21141003901~ <211410039@o365.tku.edu.tw>
2023-04-13 22:00:30 +0800

    W09-P5: Mega Menu deploy to Vercel

--- js/classdemo_39.js
+++ js/classdemo_39.js
@@ -1,11 +1,32 @@
 const showClassDemo = (week) => {
-    const showDemo = document.querySelector('.demo-section');
+    const showDemo = document.querySelector('.banner-section');
     switch(week){
         case 'w1':
             showDemo.innerHTML = `<iframe src='./demo/w01_39/index.html' width="100%" height="100%"/>`;
             break;
         case 'w2':
-
+            showDemo.innerHTML = `<iframe src='./demo/w02_39-tictactoe/tictactoe_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w3':
+            showDemo.innerHTML = `<iframe src='./demo/w03_39-review/index_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w4':
+            showDemo.innerHTML = `<iframe src='./demo/w04_39-menu/index_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w5':
+            showDemo.innerHTML = `<iframe src='./demo/w05_39-menu/index_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w6':
+            showDemo.innerHTML = `<iframe src='./demo/w06_39-array/p2_39/p2_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w9-1':
+            showDemo.innerHTML = `<iframe src='./demo/w09_39-midrep/p1_bmi_39/p1_39.html' width="100%" height="100%"/>`;
+            break;
+        case 'w9-2':
+            showDemo.innerHTML = `<iframe src='./demo/w09_39-midrep/p2_counter_39/index.html' width="100%" height="100%"/>`;
+            break;
+        case 'w9-3':
+            showDemo.innerHTML = `<iframe src='./demo/w09_39-midrep/p3_tour_39/index.html' width="100%" height="100%"/>`;
             break;
     }
 
