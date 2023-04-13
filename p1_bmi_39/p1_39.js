78214d9f52677be1e610ac3802a4f23d7f65cc1f
21141003901~ <211410039@o365.tku.edu.tw>
2023-04-13 20:10:08 +0800

    W09-P3: Midprep P1 answer (c)(d)

--- p1_bmi_39/p1_39.js
+++ p1_bmi_39/p1_39.js
@@ -1,6 +1,5 @@
 // let height = Number(prompt('輸入你的身高，以公分計算')) / 100;
 // let weight = Number(prompt('輸入你的體種，以公斤計算'));
-const bmi_data_39 = [];
 
 function bmiCalc_39(height, weight) {
     return weight/(height*height);
@@ -9,11 +8,11 @@ function bmiCalc_39(height, weight) {
 function bmiCalcReslt_39(height, weight){
     let bmi = bmiCalc_39(height, weight).toFixed(2);
     console.log(`For (h/w) = (${height}/${weight}), then BMI = ${bmi}`);
-}
+} // ``可填入字串，${}可將變書寫在內
 
-bmiCalcReslt_39(1.75, 55);
-bmiCalcReslt_39(1.75, 70);
-bmiCalcReslt_39(1.75, 85);
+// bmiCalcReslt_39(1.75, 55);
+// bmiCalcReslt_39(1.75, 70);
+// bmiCalcReslt_39(1.75, 85);
 
 function bmi_normal_low(height){
     return 18.5*height*height;
@@ -36,6 +35,52 @@ function bmiCalcSuggest_39(height, weight) {
     }
 }
 
-bmiCalcSuggest_39(1.75, 55);
-bmiCalcSuggest_39(1.75, 70);
-bmiCalcSuggest_39(1.75, 85);
+// bmiCalcSuggest_39(1.75, 55);
+// bmiCalcSuggest_39(1.75, 70);
+// bmiCalcSuggest_39(1.75, 85);
+
+const bmi_data_39 = [
+    {height: 1.75, weight: 55},
+    {height: 1.75, weight: 70},
+    {height: 1.75, weight: 85},
+];
+
+bmi_data_39.forEach(item=>{
+    bmiCalcSuggest_39(item.height, item.weight);
+})
+
+
+let lowerCount = 0;
+let normalCount = 0;
+let higherCount = 0;
+
+const bmi_data2_39 = [
+    {height: 1.75, weight: 55},
+    {height: 1.75, weight: 70},
+    {height: 1.75, weight: 85},
+    {height: 1.75, weight: 50},
+    {height: 1.75, weight: 75},
+    {height: 1.75, weight: 90},
+    {height: 1.75, weight: 80},
+    {height: 1.75, weight: 65},
+    {height: 1.75, weight: 73},
+    {height: 1.75, weight: 72},
+];
+
+bmi_data2_39.forEach(item=>{
+    const bmi = bmiCalc_39(item.height, item.weight);
+    if( bmi < 18.5){
+        lowerCount ++;
+    }else if (bmi <= 24){
+        normalCount ++;
+    }else{
+        higherCount ++;
+    }
+})
+
+console.log(`
+    bmi summary:
+        lower: ${lowerCount}
+        normal: ${normalCount}
+        higher: ${higherCount}
+`)
