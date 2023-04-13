4fd7db5e99152d8b5293f6cdf52b054446e34aa3
21141003901~ <211410039@o365.tku.edu.tw>
2023-04-13 21:33:26 +0800

    add mega menu to vercel

---
+++ js/classdemo_39.js
@@ -0,0 +1,12 @@
+const showClassDemo = (week) => {
+    const showDemo = document.querySelector('.demo-section');
+    switch(week){
+        case 'w1':
+            showDemo.innerHTML = `<iframe src='./demo/w01_39/index.html' width="100%" height="100%"/>`;
+            break;
+        case 'w2':
+
+            break;
+    }
+
+}
