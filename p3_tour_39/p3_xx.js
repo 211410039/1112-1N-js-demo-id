5b7d19ec0e0da6294f3c627eac570093ee55bff4
21141003901~ <211410039@o365.tku.edu.tw>
2023-04-13 20:43:04 +0800

    W09-P4: Midprep P3 answer

--- p3_tour_39/p3_xx.js
+++
@@ -1,9 +0,0 @@
-let tours_xx = [];
-
-const displayTours_xx = ({ tours }) => {};
-
-const fetchTours_xx = async () => {};
-
-window.addEventListener('DOMContentLoaded', () => {
-  fetchTours_xx(url);
-});
