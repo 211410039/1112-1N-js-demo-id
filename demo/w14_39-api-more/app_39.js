b4824f74b38e5037ce3784a47ce95c277aedaea8
211410039 <211410039@o365.tku.edu.tw>
2023-05-18 19:51:34 +0800

    W14-P2: Get data from MealDB, SportDB, AudioDB one by one as in P1

--- demo/w14_39-api-more/app_39.js
+++ demo/w14_39-api-more/app_39.js
@@ -1,14 +1,14 @@
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', async () => {
-  const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';//注意要打上http://
+  const url = 'https://theaudiodb.com/api/v1/json/2/album.php?i=112024';//注意要打上http://
 
 
 try {
 	const response = await fetch(url);
 	const data = await response.json();
-	console.log('data', data.drinks);
-  displayItems(data.drinks);
+	console.log('data', data.album);
+  displayItems(data.album);
 } catch (error) {
 	console.error(error);
 }
@@ -17,8 +17,8 @@ try {
 const displayItems = (items) => {
   const displayData = items
     .map((item) => {
-      const { strDrink } = item;
-      return `<p>${strDrink}</p>`;
+      const { strAlbum } = item;
+      return `<p>${strAlbum}</p>`;
     })
     .join('');
   const element = document.createElement('div');
